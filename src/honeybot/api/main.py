23d7a3b948bb76df6607321494320446eee610d7
OussamaERrafif <oussamaerra2002@gmail.com>
2024-09-05 16:00:09 +0100

    Fixed #195 issue

--- src/honeybot/api/main.py
+++ src/honeybot/api/main.py
@@ -27,83 +27,6 @@ BOT CONNECTION SETUP
 
 class BotCore:
     """
-    The core class for the bot functionality.
-
-    Args:
-        info (dict): A dictionary containing information about the bot.
-        password (str, optional): The password for the bot. Defaults to "".
-
-    Attributes:
-        info (dict): A dictionary containing information about the bot.
-        configs (dict): A dictionary containing the bot configurations loaded from a TOML file.
-        settings_path (str): The path to the settings file.
-        root_path (str): The root path of the bot.
-        server_url (str): The URL of the server the bot connects to.
-        port (int): The port number for the server connection.
-        name (str): The name of the bot.
-        owners (list): A list of usernames who are the owners of the bot.
-        password (str): The password for the bot.
-        friends (list): A list of usernames who are friends of the bot.
-        autojoin_channels (list): A list of channels the bot automatically joins.
-        downloaded_plugins_to_load (list): A list of downloaded plugins to load.
-        required_modules (list): A list of required modules for the bot.
-        time (float): The current time.
-        irc (socket.socket): The socket object for the IRC connection.
-        is_listen_on (int): Flag indicating whether the bot is listening for incoming messages.
-        domain (str): The domain of the server URL.
-        sp_command (str): The special command for the bot.
-        plugins (list): A list of loaded plugins.
-        core_plugins (list): A list of core plugins.
-
-    Methods:
-        message_info(s): Parses a message string and returns a dictionary containing information about the message.
-        bot_info(): Returns a dictionary containing information about the bot.
-        methods(): Returns a dictionary of methods used by the bot.
-        send(msg): Sends a raw message to the server.
-        send_target(target, msg): Sends a message to a specific target (channel or user).
-        join(channel): Joins a channel.
-        is_valid_plug_name(name): Checks if a plugin name is valid.
-        print_running_infos(): Prints the running information of the bot.
-        load_plugins_from_folder(category_folder, from_conf, from_dir): Loads plugins from a specific folder.
-        load_plugins(): Loads the plugins specified in the configuration.
-        run_plugins(incoming): Runs the loaded plugins for an incoming message.
-        core_commands_parse(incoming): Parses the core commands for an incoming message.
-        connect(): Connects to the server.
-        identify(): Sends the identification command to the server.
-        greet(): Sends the initial commands to the server for bot registration.
-        pull(): Listens for incoming messages from the server.
-        quit(): Sends the quit command to the server.
-        stay_alive(incoming): Handles the stay alive functionality for the bot.
-        registered_run(): Runs the bot as a registered bot.
-        unregistered_run(): Runs the bot as an unregistered bot.
-    """
-
-    def __init__(self, info, password=""):
-        self.info = info
-        with open(info["toml_path"], "rb") as f:
-            self.configs = tomli.load(f)
-        self.settings_path = self.info["settings_path"]
-        self.root_path = self.info["cwd"]
-        self.server_url = self.configs["INFO"]["server_url"]
-        self.port = int(self.configs["INFO"]["port"])
-        self.name = self.configs["INFO"]["name"]
-        self.owners = self.configs["USERNAMES"]["owners"]
-        self.password = password
-        self.friends = self.configs["USERNAMES"]["friends"]
-        self.autojoin_channels = self.configs["INFO"]["autojoin_channels"]
-        self.downloaded_plugins_to_load = self.configs["PLUGINS"]["downloaded"]
-        self.required_modules = get_requirements()
-        self.time = time.time()
-
-        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.is_listen_on = 1
-        dom = self.server_url.split(".")
-        self.domain = ".".join(dom[-2:])
-        self.sp_command = "hbot"
-        self.plugins = []
-        self.core_plugins = []
-class BotCore:
-    """
     The main class that represents the core functionality of the bot.
 
     Args:
@@ -155,33 +78,6 @@ class BotCore:
         unregistered_run(): Runs the bot as an unregistered bot.
     """
 
-    def __init__(self, info, password=""):
-        self.info = info
-        with open(info["toml_path"], "rb") as f:
-            self.configs = tomli.load(f)
-        self.settings_path = self.info["settings_path"]
-        self.root_path = self.info["cwd"]
-        self.server_url = self.configs["INFO"]["server_url"]
-        self.port = int(self.configs["INFO"]["port"])
-        self.name = self.configs["INFO"]["name"]
-        self.owners = self.configs["USERNAMES"]["owners"]
-        self.password = password
-        self.friends = self.configs["USERNAMES"]["friends"]
-        self.autojoin_channels = self.configs["INFO"]["autojoin_channels"]
-        self.downloaded_plugins_to_load = self.configs["PLUGINS"]["downloaded"]
-        self.required_modules = get_requirements()
-        self.time = time.time()
-
-        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.is_listen_on = 1
-        dom = self.server_url.split(".")
-        self.domain = ".".join(dom[-2:])
-        self.sp_command = "hbot"
-        self.plugins = []
-        self.core_plugins = []
-
-    # Rest of the code...
-class BotCore:
     def __init__(self, info, password=""):
         self.info = info
         with open(info["toml_path"], "rb") as f:
@@ -434,29 +330,22 @@ class BotCore:
         print(output.status("x"), "Joined channels:", ", ".join(self.autojoin_channels))
 
     def pull(self):
-            """
-            Listens to incoming messages and parses them using core_commands_parse method.
-            """
-            print(output.status("i"), "Listening to incoming messages")
-            while self.is_listen_on:
-                try:
-                    data = self.irc.recv(2048).decode("UTF-8", errors="replace")
-                    for line in data.split("\r\n"):
-                        if line != "":
-                            self.core_commands_parse(line)
+        """
+        Listens to incoming messages and parses them using core_commands_parse method.
+        """
+        print(output.status("i"), "Listening to incoming messages")
+        while self.is_listen_on:
+            try:
+                data = self.irc.recv(2048).decode("UTF-8", errors="replace")
+                for line in data.split("\r\n"):
+                    if line != "":
+                        self.core_commands_parse(line)
 
-                except KeyboardInterrupt:
-                    self.is_listen_on = False
-                    self.quit()
-                except Exception as e:
-                    # logger.error(e)
-                    # print(e)
-                    # logger.debug("there was an error")
-                    # logger.debug(data)
-                    # logger.debug("!!")
-                    # logger.debug(line)
-                    # logger.debug("-" * 50)
-                    pass
+            except KeyboardInterrupt:
+                self.is_listen_on = False
+                self.quit()
+            except Exception as e:
+                pass
 
     def quit(self):
         self.send(commands.quit())
